c5afe3f6fbc6d1ad25204126ee04178d3b10dcfe
Michael Bestas <mkbestas@gmail.com>
2023-04-04 22:10:12 +0300

    Initial CalyxOS makefile
    Change-Id: Ieefcb32c1619d7623f788bc910d16811b881b8be

---
+++ calyx_hawao.mk
@@ -0,0 +1,26 @@
+#
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common stuff
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
+
+# Inherit from device
+$(call inherit-product, device/motorola/hawao/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := calyx_hawao
+PRODUCT_DEVICE := hawao
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g42
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="hawao_g-user 13 T2SES33.73-23-2-15 ed051c-cd01b release-keys" \
+    BuildFingerprint=motorola/hawao_g/hawao:13/T2SES33.73-23-2-15/ed051c-cd01b:user/release-keys \
+    DeviceProduct=hawao_g
