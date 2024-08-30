07014d2aa71975a4d12f2182c38947bc0c7421f7
Michael Bestas <mkbestas@gmail.com>
2024-08-30 16:03:23 +0300

    calyx: Update to T2SES33.73-23-2-8
    Change-Id: I3d9df076ca82cd8a8d7f4df4bdfaf06383313f69

--- calyx_hawao.mk
+++ calyx_hawao.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := moto g42
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-BUILD_FINGERPRINT := "motorola/hawao_g/hawao:13/T2SES33.73-23-2-5/3f59c4-36755e:user/release-keys"
+BUILD_FINGERPRINT := "motorola/hawao_g/hawao:13/T2SES33.73-23-2-8/5616a2-1c4eb8:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=hawao_g \
-    PRIVATE_BUILD_DESC="hawao_g-user 13 T2SES33.73-23-2-5 3f59c4-36755e release-keys"
+    PRIVATE_BUILD_DESC="hawao_g-user 13 T2SES33.73-23-2-8 5616a2-1c4eb8 release-keys"
